1de2d220ec734e896cf535f864d6e779cbb445d2
dmmkawarada <ideal_gate@icloud.com>
2019-12-25 08:06:27 +0000

    create customer file

--- db/migrate/20191222075514_add_lastkana_to_customer.rb
+++
@@ -1,5 +0,0 @@
-class AddLastkanaToCustomer < ActiveRecord::Migration[5.2]
-  def change
-    add_column :customers, :lastkana, :string
-  end
-end
--- db/migrate/20191222075854_add_customer_status_to_customer.rb
+++
@@ -1,5 +0,0 @@
-class AddCustomerStatusToCustomer < ActiveRecord::Migration[5.2]
-  def change
-    add_column :customers, :customer_status, :integer
-  end
-end
