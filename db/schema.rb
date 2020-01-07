a482778419935e1bcfd393ea262fe9357d1af62a
dmmkawarada <ideal_gate@icloud.com>
2020-01-07 19:44:24 +0000

    [change]link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_072044) do
+ActiveRecord::Schema.define(version: 2020_01_07_073813) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -102,9 +102,9 @@ ActiveRecord::Schema.define(version: 2020_01_07_072044) do
   end
 
   create_table "taxes", force: :cascade do |t|
-    t.integer "tax"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "tax"
   end
 
 end
