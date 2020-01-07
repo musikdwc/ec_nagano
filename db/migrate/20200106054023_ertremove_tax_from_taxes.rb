ceac7857b8db25050d70353e40d93b99d687d874
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2020-01-07 06:23:51 +0000

    [ADD/Update]header/about

--- db/migrate/20200106054023_ertremove_tax_from_taxes.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTaxFromTaxes < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :taxes, :tax, :integer
-  end
-end
--- db/migrate/20200106054732_add_taxpast_to_taxes.rb
+++
@@ -1,4 +0,0 @@
-class AddTaxToTaxes < ActiveRecord::Migration[5.2]
-  def change
-    add_column :taxes, :tax, :decimal
-end
