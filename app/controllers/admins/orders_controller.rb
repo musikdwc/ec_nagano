77226717eff618f972e7a86277ca2c54dac07094
dmmkawarada <ideal_gate@icloud.com>
2020-01-07 18:41:14 +0000

    [finish]customer

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -2,13 +2,29 @@ class Admins::OrdersController < ApplicationController
 	def index
 		@orders = Order.all
 	end
-	def show
-		@order = Order.find(params[:id])
-	end
 	def update
 		@order = Order.find(params[:id])
-		@order.update(order_params)
-        redirect_to admins_customer_path
+		@order_details = @order.order_details
+        # @order.update(order_params)
+        @order.update(order_status: params[:order][:order_status].to_i)
+	        if @order_details.where(production_status: 2).any?
+	        	  @order.update(order_status: 2)
+	        else
+	        end
+	        if @order.order_status_before_type_cast == 1
+	        	@order_details.each do |item|
+	           		item.update(production_status: 1)
+	       		end
+	        end
+	        if @order_details.count == @order_details.where(production_status: 3).count
+	        	  @order.update(order_status: 3)
+	        else
+	        end
+
+        redirect_to admins_order_path
+	end
+	def show
+		@order = Order.find(params[:id])
 	end
 	def top
 		from = Time.current.beginning_of_day
@@ -17,7 +33,7 @@ class Admins::OrdersController < ApplicationController
 
 	end
 	def order_params
-		params.permit(:deliver_to, :shipping_name, :order_postshipping_cost, :order_status, :payment_method, :created_at)
+		params.permit(:deliver_to, :shipping_name, :order_postshipping_cost, :order_status, :payment_method, :created_at, order_detail_attributes: [:id, :production_status])
 	end
 	def order_detail_params
 		params.require(:order_detail).permit(:ordered_price, :ordered_item_name, :ordered_item_count, :production_status)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,4 @@
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 
-Admin.create!(
-	email: 'admin@test.com',
-    password: '165432'
-)
-Customer.create(email: 'test1@test.com', password: '111111')
-Customer.create(email: 'test2@test.com', password: '222222')
-Customer.create(email: 'test3@test.com', password: '333333')
-Customer.create(email: 'test4@test.com', password: '444444')
-
-Admin.create!(
-    {
-      email: 'admin1@test.com',
-      password: '111111'
-    }
-)
-
-
-
 Tax.create(tax: 1.1)
